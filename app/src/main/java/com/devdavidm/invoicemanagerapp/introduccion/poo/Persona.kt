ab75f82d9c6bb157c0110a39132af0362e4a4ab6
juanda <devdavidm@gmail.com>
2024-03-12 18:47:35 -0500

    Cambio en clase Persona

--- app/src/main/java/com/devdavidm/invoicemanagerapp/introduccion/poo/Persona.kt
+++ app/src/main/java/com/devdavidm/invoicemanagerapp/introduccion/poo/Persona.kt
@@ -1,8 +1,10 @@
 package com.devdavidm.invoicemanagerapp.introduccion.poo
 
-class Persona(var nombre: String, var edad: Int, var estatura: Double, var sexo: Boolean) {
+class Persona(var nombre: String="", var edad: Int, var estatura: Double, var sexo: Boolean) {
+    private val planetaDeOrigen = "Tierra"
     fun printValues(){
         println()
+        println("Planeta: ${this.planetaDeOrigen}")
         println("Nombre: ${this.nombre}")
         println("Edad: ${this.edad}")
         println("Estatura: ${this.estatura}")
